106e9b222e995d260110912f1b6f5776bfe61844
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2024-09-30 10:04:56 +0200

    Pong: controlls for bars, ball movement, etc

--- pong.rb
+++ pong.rb
@@ -1,9 +1,12 @@
 require 'gosu'
 
 class Player
-    def initialize(xCordinate, yCordinate)
-        @bar = PongBar.new(xCordinate, yCordinate)
+    def initialize()
         @score = 0
+    end
+
+    def createBar(x, y, bool)
+        PongBar.new(x, y, bool)
     end
 
     def update
@@ -12,25 +15,100 @@ class Player
 
 end
 
-class PongBall
+class PongBall < Player
     def initialize
         @image = Gosu::Image.new("./media/pongball.png")
+        @xCord = 312
+        @yCord = 312
+        @xDir = rand(10..39)*0.1
+        @yDir = (4**2 - (@xDir)**2)**0.5
+        @top = @yCord
+        @bottom = @yCord + 16
+        @left = @xCord
+        @right = @xCord + 16
+    end
+
+    def update
+        if (@top <= 16 || @bottom >= 626)
+            @yDir *= -1
+        end
+
+        if (@left <= 16)
+            ballHitWall(false)
+        elsif (@right >= 626)
+            ballHitWall(true)
+        end
+
+        @xCord += @xDir
+        @yCord += @yDir
+        @top = @yCord
+        @bottom = @yCord + 16
+        @left = @xCord
+        @right = @xCord + 16
     end
 
     def draw
-        @image.draw(312, 312, 0, 3, 3)
+        @image.draw(@xCord, @yCord)
+    end
+
+    def ballHitWall (playerOneWinBool)
+        if !playerOneWinBool
+            
+        else
+            
+        end
+        initialize
+    end
+
+    def getPosition
+        {
+            "top" => @top,
+            "bottom" => @bottom,
+            "left" => @left,
+            "right" => @right
+        }
     end
 end
 
 class PongBar
     
-    def initialize(xCordinate, yCordinate)
+    def initialize(xCordinate, yCordinate, isPlayerOne)
         @image = Gosu::Image.new("./media/pongbar.png")
         @xCord = xCordinate
         @yCord = yCordinate
+        @top = @yCord
+        @bottom = @yCord + 64
+        @side = @xCord
+        if isPlayerOne
+            @side += 8
+        end
+    end
+
+    def moveUp
+        if @top >= 16+8
+            @yCord -= 8
+        end
 
     end
 
+    def moveDown
+        if @bottom <= 640-16-8
+            @yCord += 8
+        end
+    end
+
+    def getPosition
+        {
+            "top" => @top,
+            "bottom" => @bottom,
+            "side" => @side
+        }
+    end
+
+    def update
+        @top = @yCord
+        @bottom = @yCord + 64
+    end
 
     def draw
         @image.draw(@xCord, @yCord, 0)
@@ -42,22 +120,43 @@ end
 
 class PongGame < Gosu::Window
 
-    #konstruktor
     def initialize
         super 640, 640
         self.caption = "Pong Thong"
         xCord1 = 64
-        xCord2 = 576-60
-        yCord = 288
+        xCord2 = 640-64
+        yCord = 272
         @ball = PongBall.new
-        @bar1 = PongBar.new(xCord1, yCord)
-        @bar2 = PongBar.new(xCord2, yCord)
+        @player1 = Player.new
+        @player2 = Player.new
+        @bar1 = @player1.createBar(xCord1, yCord, true)
+        @bar2 = @player2.createBar(xCord2, yCord, false)
         @background = Gosu::Image.new("./media/pongbackground.png")
     end
 
 
     def update
-        
+        @ball.update
+        @bar1.update
+        @bar2.update
+
+        @ball.getPosition
+        @bar1.getPosition
+        @bar2.getPosition
+
+        if Gosu.button_down? Gosu::KB_UP
+            @bar2.moveUp
+        end
+        if Gosu.button_down? Gosu::KB_W
+            @bar1.moveUp
+        end
+        if Gosu.button_down? Gosu::KB_DOWN
+            @bar2.moveDown
+        end
+        if Gosu.button_down? Gosu::KB_S
+            @bar1.moveDown
+        end
+
     end
 
     def draw
